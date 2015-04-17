ee0d55d969dc8685cfcf46f5bccce11029fb31ee
Trevor Caverly <trevorcaverly@gmail.com>
2015-04-16 22:37:03 -0700

    [feat] Simple image upload function with cloudinary

---
+++ server/cloudinary/upload.js
@@ -0,0 +1,14 @@
+var cloudAPI = require("./apikey");
+var cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: cloudAPI.cloudname,
+  api_key: cloudAPI.key,
+  api_secret: cloudAPI.secret
+});
+
+exports.uploadFromFilePath = function(path, cb) {
+  cloudinary.uploader.upload(path, function(result) {
+    cb(result);
+  });
+};
